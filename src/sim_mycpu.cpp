82e3453b9ce2a4042bbb5035490879c84df78549
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-08-29 16:05:02 +0800

    add system_test

--- src/sim_mycpu.cpp
+++ src/sim_mycpu.cpp
@@ -89,6 +89,70 @@ void uart_input(uart8250 &uart) {
     }
 }
 
+void system_test(Vmycpu_top *top, axi4_ref <32,32,4> &mmio_ref) {
+    axi4     <32,32,4> mmio_sigs;
+    axi4_ref <32,32,4> mmio_sigs_ref(mmio_sigs);
+    axi4_xbar<32,32,4> mmio;
+
+    // uart8250 at 0x1fe40000 (APB)
+    uart8250 uart;
+    std::thread *uart_input_thread = new std::thread(uart_input,std::ref(uart));
+    assert(mmio.add_dev(0x1fe40000,0x10000,&uart));
+
+    // SPI Flash as memory at 0x1fc00000
+    mmio_mem spi_flash(0x100000, "../supervisor-mips32/kernel/kernel.bin");
+    assert(mmio.add_dev(0x1fc00000,0x100000,&spi_flash));
+
+    // DRAM(128M) at 0x00000000
+    mmio_mem dram(0x8000000);
+    assert(mmio.add_dev(0x0,0x8000000,&dram));
+
+    // connect Vcd for trace
+    VerilatedVcdC vcd;
+    if (trace_on) {
+        top->trace(&vcd,0);
+        vcd.open("trace.vcd");
+    }
+
+    // init and run
+    top->aresetn = 0;
+    unsigned long ticks = 0;
+    while (!Verilated::gotFinish() && sim_time > 0 && running) {
+        top->eval();
+        ticks ++;
+        if (trace_pc && top->debug_wb_rf_wen) printf("pc = %lx\n", top->debug_wb_pc);
+        if (trace_on) {
+            vcd.dump(ticks);
+            sim_time --;
+        }
+        if (ticks == 9) top->aresetn = 1;
+        top->aclk = 1;
+        // posedge
+        mmio_sigs.update_input(mmio_ref);
+        top->eval();
+        ticks ++;
+        if (top->aresetn) {
+            mmio.beat(mmio_sigs_ref);
+            while (uart.exist_tx()) {
+                char c = uart.getc();
+                printf("%c",c);
+                fflush(stdout);
+            }
+        }
+        mmio_sigs.update_output(mmio_ref);
+        if (trace_pc && top->debug_wb_rf_wen) printf("pc = %lx\n", top->debug_wb_pc);
+        if (trace_on) {
+            vcd.dump(ticks);
+            sim_time --;
+        }
+        top->ext_int = uart.irq() << 2;
+        top->aclk = 0;
+    }
+    if (trace_on) vcd.close();
+    top->final();
+    pthread_kill(uart_input_thread->native_handle(),SIGKILL);
+}
+
 bool func_run(Vmycpu_top *top, axi4_ref <32,32,4> &mmio_ref, uint32_t delay) {
     axi4     <32,32,4> mmio_sigs;
     axi4_ref <32,32,4> mmio_sigs_ref(mmio_sigs);
